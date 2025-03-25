dab61a439306cc2020a38e8da301bb12871ffe27
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-03-25 14:27:21 +0100

    implemented ex1 stack

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -1,30 +1,40 @@
 package tasks.adts
 
-import u03.Sequences.*
-import u03.Optionals.*
+import u03.extensionmethods.Sequences.{Sequence, *}
+import u03.Optionals.Optional
+import u03.Optionals.Optional.Just
+import u03.extensionmethods.Sequences.Sequence.{Cons, nil}
 
 /*  Exercise 3: 
  *  Implement a Stack ADT
  *  Suggestion: 
  *  - push adds an element and returns the new stack
  *  - pop returns:
- *  -- empty optional is stack is empty
+ *  -- empty optional if stack is empty
  *  -- a pair of top of the stack and the new stack after removal if not empty
  */
 object Ex3Stacks:
 
   trait StackADT:
     type Stack[A]
+
     def empty[A]: Stack[A] // factory
+
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A]
       def pop(): Optional[(A, Stack[A])]
       def asSequence(): Sequence[A]
 
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    type Stack[A] = Sequence[A]
+
+    def empty[A]: Stack[A] = nil()
+
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
-      def pop(): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def push(a: A): Stack[A] = Cons(a, stack)
+      def pop(): Optional[(A, Stack[A])] = stack match
+        case Cons(h, t) if h != nil() => Just(h, t)
+        case _ => Optional.Empty()
+      def asSequence(): Sequence[A] = stack match
+        case Cons(h, t) => stack
+        case _ => nil()
--- src/test/scala/tasks/adts/StackTest.scala
+++ src/test/scala/tasks/adts/StackTest.scala
@@ -3,7 +3,7 @@ package tasks.adts
 import org.junit.*
 import org.junit.Assert.*
 import tasks.adts.Ex3Stacks.StackImpl
-import u03.Sequences.Sequence
+import u03.extensionmethods.Sequences.Sequence
 import u03.Optionals.Optional
 
 /* Tests should be clear, but note they are expressed independently of the 
@@ -16,22 +16,23 @@ class Stacktest:
   val stack = StackImpl
 
   import stack.*
+
   @Test def testEmptyStackHasNoElements() =
     assertEquals(Sequence.Nil(), empty[Int].asSequence())
-  
+
   @Test def testPushAddsElementToStack() =
     assertEquals(Sequence.Cons(10, Sequence.Nil()), empty[Int].push(10).asSequence())
-  
+
   @Test def testPopOnEmptyStackReturnsEmpty() =
     assertEquals(Optional.Empty(), empty[Int].pop())
-  
+
   @Test def testPopOnStackWithOneElementReturnsElementAndEmptyStack() =
     assertEquals(Optional.Just((10, empty[Int])), empty[Int].push(10).pop())
-  
+
   @Test def testPushMultipleElementsAndVerifyOrder() =
     val stack = empty[Int].push(10).push(20).push(30)
     assertEquals(Sequence.Cons(30, Sequence.Cons(20, Sequence.Cons(10, Sequence.Nil()))), stack.asSequence())
-  
+
   @Test def testPopMultipleElementsMaintainsOrder() =
     val stack = empty[Int].push(10).push(20)
     val popResult = stack.pop()
